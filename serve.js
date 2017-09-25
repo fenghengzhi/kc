cc2e107e8f321b9eee947a6de57196a0e98599c0
fenghengzhi <sr&zMpfr&Qq$Zzy6jXVW>
2017-09-25 17:53:28 +0800

    更新

---
+++ serve.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var browserSync = require('browser-sync');
+
+browserSync.init({
+    serveStatic: ['.'],
+    proxy: '101.226.6.177:8089',
+    open: 'local',
+    startPath: '/haikou/首页.html',
+    ghostMode: false
+});
